625d5b3544921805f8d679d92116670f392a42ad
Jon Schemmel <Jon@Jons-MacBook-Pro.local>
2021-02-21 23:00:26 -0600

    Added in test data.  Also added in CardList component.

--- src/index.js
+++ src/index.js
@@ -9,19 +9,36 @@ import './index.css';
 // List
 // Starting from the bottom up.
 
+
+const testData = [
+	{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+	{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
 // * Make below block a class component *
 // const App = ({title}) => (
 //   <div className="header">{title}</div>
 // );
 
+const CardList = (props) => {
+  return(
+    <div>
+      <Card />
+    </div>
+  );
+}
+
 class Card extends React.Component {
   render() {
+    const profile = testData[0];
     return (
       <div className="github-profile">
-        <img src="https://placehold.it/75" />
+        <img src={profile.avatar_url } />
         <div className="info">
-          <div className="name">Name Here..</div>
-          <div className="company">Company Here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
@@ -38,7 +55,7 @@ class App extends React.Component {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <CardList />
       </div>
     )
   }
